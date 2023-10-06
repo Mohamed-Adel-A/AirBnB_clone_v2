51cd8dd35c39f46779b6b08d70756e522841c9be
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-06 05:24:59 +0300

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,13 +1,13 @@
 # Setup server using Puppet
 
 exec { 'setting up server':
-  command => 'sudo apt-get update;
-  sudo apt-get install nginx -y;
-  mkdir -p /data/web_static/releases/test/;
-  mkdir -p /data/web_static/shared/;
-  echo "Test page" > /data/web_static/releases/test/index.html;
-  ln -sf /data/web_static/releases/test/ /data/web_static/current;
-  chown -R ubuntu:ubuntu /data;
+  command => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo mkdir -p /data/web_static/releases/test/;
+  sudo mkdir -p /data/web_static/shared/;
+  echo "Test page" | sudo tee /data/web_static/releases/test/index.html;
+  sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
+  sudo chown -R ubuntu:ubuntu /data;
   sudo sed -i "38i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n}" /etc/nginx/sites-available/default;
   sudo service nginx restart;'
   provider => shell,
