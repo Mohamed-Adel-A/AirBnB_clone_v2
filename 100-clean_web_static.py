7a433822b49fe77e41a82f162efb97ee9c2f842c
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-06 05:41:39 +0300

    Create 100-clean_web_static.py

---
+++ 100-clean_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ a Fabric script (based on the file 3-deploy_web_static.py)
+ that deletes out-of-date archives,
+ using the function do_clean
+"""
+
+
+def do_clean(number=0):
+    """
+    Fuction that delete out-of-date archvies
+    """
+    pass
