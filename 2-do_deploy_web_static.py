8602720e0abbee74da816d980cf31a4d4b9d4a15
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-06 03:06:12 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,29 +20,32 @@ def do_deploy(archive_path):
     archive = archive_path.split('/')[-1]
     filename = archive.split('.')[0]
 
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, "/tmp/")
-    # create the folder to uncopress the archive to it
-    run("mkdir -p /data/web_static/releases/{}/".fomat(filename))
-    # Uncompress the archive
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(archive, filename))
-    # Delete the archive from the web server
-    run("rm /tmp/{}".format(archive))
-
-    # move the contenet of web_static up
-    run("mv /data/web_static/releases/{}/web_static/* \
-    /data/web_static/releases/{}/"
-       .format(filename, filename))
-    # delete web_static dir
-    run("rm -rf /data/web_static/releases/{}/web_static"
-       .foramt(filename))
-
-    # Delete the symbolic link /data/web_static/current
-    run("rm -rf /data/web_static/current")
-    # Create a new the symbolic link /data/web_static/current
-    # on the web server, 
-    # linked to the new version of your code
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # create the folder to uncopress the archive to it
+        run("mkdir -p /data/web_static/releases/{}/".fomat(filename))
+        # Uncompress the archive
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive, filename))
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(archive))
     
+        # move the contenet of web_static up
+        run("mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/"
+           .format(filename, filename))
+        # delete web_static dir
+        run("rm -rf /data/web_static/releases/{}/web_static"
+           .foramt(filename))
     
-    
+        # Delete the symbolic link /data/web_static/current
+        run("rm -rf /data/web_static/current")
+        # Create a new the symbolic link /data/web_static/current
+        # on the web server, 
+        # linked to the new version of your code
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
+        return True
+    except:
+        return False
