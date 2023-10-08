9c7c84a8112c8c22482e68e6e227084ec26b18a0
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-09 01:44:19 +0300

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,6 +10,6 @@ exec {'install':
   echo "Test page" | sudo tee /data/web_static/releases/test/index.html;
   sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
   sudo chown -R ubuntu:ubuntu /data;
-  sudo sed -i "38i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n}" /etc/nginx/sites-available/default;
+  sudo sed -i "38i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html index.htm;\n}" /etc/nginx/sites-available/default;
   sudo service nginx restart;',
 }
