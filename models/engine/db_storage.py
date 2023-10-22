4cf74b4f4dbb86045a446c5162da888a7aaa4e3e
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-22 09:55:19 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -89,3 +89,11 @@ class DBStorage():
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """
+        call remove() method
+        on the private session attribute (self.__session) 
+        or close() on the class Session
+        """
+        return (self.__session.remove())
