dbb6c1e479bdaaaa1fbe9dbb0d5a681d97d6f320
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-22 09:59:17 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -93,7 +93,7 @@ class DBStorage():
     def close(self):
         """
         call remove() method
-        on the private session attribute (self.__session) 
+        on the private session attribute (self.__session)
         or close() on the class Session
         """
         return (self.__session.close())
