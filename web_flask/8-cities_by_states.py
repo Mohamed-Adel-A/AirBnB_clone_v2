612d91954facb9877eb0985e9b7d392a439c7c44
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-10-22 11:56:33 +0300

    Create 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+- /states_list: display a HTML page: (inside the tag BODY)
+    H1 tag: “States”
+    UL tag: with the list of all State objects
+    present in DBStorage sorted by name (A->Z)
+    LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def states_list_route():
+    """
+    display a HTML page of state list
+    """
+    states_list = storage.all(State)
+    return (render_template("7-states_list.html", states_list=states_list))
+
+
+@app.route("/cities_by_states")
+def cities_by_states_route():
+    """
+    display a HTML page of state list
+    """
+    states_list = storage.all(State)
+    return (render_template("8-cities_by_states.html", states_list=states_list))
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    close file or db connection
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
